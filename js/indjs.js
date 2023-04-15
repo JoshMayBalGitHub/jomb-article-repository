7639b750ab004c10b5ec39efa18f0adffaadb97c
JoshMayBal <joshuamiguelcalara@gmail.com>
2023-04-15 22:58:43 +0800

    finish asb and revamp css and aricle navigaton

--- js/indjs.js
+++ js/indjs.js
@@ -7,11 +7,8 @@ Still, nothing. - Joshua, 02-18-23
 Yeah nah, i am still insane - Joshua, 03-14-23
 For now, idk what i can do, this is the best method.. -Joshua
 *//////////////////////////
-function Jombarticle1() {
-    window.location.href = "mt.html";
-   }
-function Jombarticle2() {
-    window.location.href = "smi.html";
+function Jombarticle() {
+    window.location.href = "article-navigator.html";
    }
 function Jombspecialscihi() {
     window.location.href = "scihi-navigator.html";
